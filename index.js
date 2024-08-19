43eb1f008044e21d0460f8e66c8742b36e002b87
Chrismiweb <chrismibiteso@gmail.com>
2024-08-18 18:01:26 -0700

    hotel booking backend completed

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.use(express.json())
 // use express file upload
 app.use(expressFilUpload());
 
+// use body-parser
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get('/', (req,res)=>{
+    res.send("this app is running")
+})
+
 // connect mongoose 
     const connectString = process.env.connectString
     async function connectMongoose (){
@@ -83,16 +90,50 @@ app.use(expressFilUpload());
 
    });
 
+   
+    // to get all available hotels
+   app.get('/api/v1/getAllHotel', async(req, res)=>{
+        const getAllHotel = await hotelModel.find()
 
+        if(!getAllHotel) {
+            res.status(404).json({error: "hotel not found"})
+        }
+        res.json({getAllHotel})
+   })
+
+    // to get individual hotel using the hotel Name
+    app.get('/api/v1/oneHotel/:hotelName' , async(req,res)=>{
+        const {hotelName} = req.params
+        const findHotel = await hotelModel.findOne({hotelName})
+
+        if(!findHotel){
+            res.status(404).json({error: "the hotel you are finding is not available"})
+        }
+        res.json({findHotel})
+    })
+
+    // to delete individual hotel using hotel name 
+    app.delete('/api/v1/deleteOneHotel/:hotelName', async(req,res)=>{
+        const {hotelName} = req.params
+        const findHotel = await hotelModel.findOneAndDelete({hotelName})
+        if(!findHotel){
+           return res.status(404).json({error: "the hotel you want to delete is not available"})
+        }
+        res.json({message: "hotel was deleted successfully"})
+    })
+
+    // to update a hotel detail
+    app.patch('/api/v1/updateHotel/:hotelName', async(req,res)=>{
+        const {hotelName} = req.params
+        const updateHotel  = await hotelModel.findOneAndUpdate({hotelName}, req.body, {runValidator: true, new:true})
+        if(!updateHotel){
+            res.status(404).json({error: "the hotel detail you want to update was not found"}, req.body, {runValidator: true, new:true})
+        }
+        res.json({message : "hotel details was updated succesfully"})
+    })
    
    
 
-// use body-parser
-app.use(bodyParser.urlencoded({extended:false}))
-
-app.get('/', (req,res)=>{
-    res.send("this app is running")
-})
 
 app.listen(port, async() => {
     console.log(`Server started on port ${port}`);
