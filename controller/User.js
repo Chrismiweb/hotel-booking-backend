612091c6a5ea4f0622c5b7a3d3b80f4c95c8dec2
Chrismiweb <chrismibiteso@gmail.com>
2024-09-16 23:58:45 -0700

    trying to add nodemailer

--- controller/User.js
+++ controller/User.js
@@ -1,6 +1,6 @@
 const userModel = require("../models/User.model")
 const bcrypt = require('bcrypt');
-
+const nodemailer = require("nodemailer");
 
 const register = async(req, res)=>{
     try {
@@ -17,6 +17,17 @@ const register = async(req, res)=>{
     const hash = bcrypt.hashSync(password, saltPassword);
 
     const registerUser = await new userModel({userName, email, password: hash})
+    if(registerUser){
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for port 465, false for other ports
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+    }
     if(!registerUser){
         return res.status(401).json({message: "unable to register this account"})
     }
